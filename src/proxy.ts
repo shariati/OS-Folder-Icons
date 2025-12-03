d710ecefdb83c4269a3e8714f2a7fb4e6d7e6426
Amin Shariati <amin.shariati@gmail.com>
2025-12-03 18:32:01 +0800

    chore: rename middleware to proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
     const hostname = req.nextUrl.hostname;
     const isStaging = hostname === 'test.hdpick.com';
 
